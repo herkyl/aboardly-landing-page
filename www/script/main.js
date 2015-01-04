267bb647ffefe6b5e272b8e287a9c3717dd4c7e9
herkyl <s.herkyl@gmail.com>
2015-01-04 16:41:48 +0200

    GA Jade

--- www/script/main.js
+++ www/script/main.js
@@ -1,4 +1,4 @@
-var getEmail, onSubmit, resetEmail, submitDone;
+var getEmail, onSubmit, resetEmail, submitDone, validateEmail;
 
 getEmail = function() {
   var fields;
@@ -12,11 +12,24 @@ resetEmail = function() {
   return fields[0].value = '';
 };
 
+validateEmail = function(email) {
+  if (/(.+)@(.+){2,}\.(.+){2,}/.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 onSubmit = function() {
-  var firebase;
+  var email, firebase;
+  email = getEmail();
+  if (!validateEmail(email)) {
+    alert("" + email + " is an invalid email");
+    return false;
+  }
   firebase = new Firebase('https://aboardly-landing.firebaseio.com/emails');
   firebase.push({
-    email: getEmail()
+    email: email
   }, submitDone);
   resetEmail();
   return false;
